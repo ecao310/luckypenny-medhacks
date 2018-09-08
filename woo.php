31d06155af388e4bed67c4b3ea15945465cf641e
maxxu05 <maxxu05@gmail.com>
2018-09-08 17:56:49 -0400

    Update and rename woo.html to woo.php

---
+++ woo.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<marquee><p>wooooooooooo.</p> </marquee>
+<marquee><p>wooooooooooo.</p> </marquee>
+<marquee><p>wooooooooooo.</p> </marquee>
+<marquee><p>wooooooooooo.</p> </marquee>
+<marquee><p>wooooooooooo.</p> </marquee>
+<marquee><p>wooooooooooo.</p> </marquee>
+<marquee><p>wooooooooooo.</p> </marquee>
+<marquee><p>wooooooooooo.</p> </marquee>
+<marquee><p>wooooooooooo.</p> </marquee>
+<marquee><p>wooooooooooo.</p> </marquee>
+
+
+Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["mom"]; ?>
+
+
+</body>
+</html>
